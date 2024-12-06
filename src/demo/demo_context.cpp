ec33103942451548010ff32bb37de4eb3e3c9432
Krzysztof Weronski <weronski@student.agh.edu.pl>
2024-12-06 21:57:58 +0100

    Add some comments to context demo

--- src/demo/demo_context.cpp
+++ src/demo/demo_context.cpp
@@ -1,3 +1,7 @@
+/* This is the recommended demo to copy from,
+ * if you'd like to add your own demo.
+ */
+
 #include "context.hpp"
 #include <iostream>
 
@@ -6,8 +10,9 @@ bool initialize(fw::context_t *ctx);
 }
 
 int main(int, char **argv) {
-  fw::context_t ctx{};
-  fw::cd_to_binary_dir(&ctx, argv[0]);
+  fw::context_t ctx{}; // This holds all gui / game related data.
+  fw::cd_to_binary_dir(&ctx,
+                       argv[0]); // To reference resources more conveniently
 
   if (!initialize(&ctx))
     return -1;
@@ -21,6 +26,11 @@ int main(int, char **argv) {
 namespace {
 bool initialize(fw::context_t *ctx) {
   ctx->event_cb_map[sf::Event::Closed] = [](auto *p) { p->window.close(); };
+  // The event map is scanned during each frame.
+  // If a registered event has a callback, that callback is executed.
+  // In this case, when the close button is pressed, the window is told to
+  // close.
+
   ctx->window.create(sf::VideoMode{640, 480}, "context demo");
 
   if (!ctx->font.load("./font/AnonymousPro-Regular.ttf", "AnonymousPro")) {
@@ -33,6 +43,8 @@ bool initialize(fw::context_t *ctx) {
       fw::load_scene(ctx, "./xml/demo_settings_menu.xml", "settings_menu") &&
       fw::load_scene(ctx, "./xml/demo_maze_level.xml", "maze_level") &&
       fw::activate_scene(ctx, "main_menu");
+  // Here we are loading the xml files that contain descriptions from which
+  // graphical objects will be assembled and linked into a tree.
 
   if (!ok)
     std::cerr << "Failed to initialize resources!" << std::endl;
